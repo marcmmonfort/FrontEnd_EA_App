674e4203738c3a2785d55d3038c2cc5e177fbd8e
Quim Moran M <marc.moran@estudiantat.upc.edu>
2023-05-25 00:24:58 +0200

    feat: addition of the navbar and the cards of the searched users in the discovery screen.

---
+++ src/infrastructure/UI/components/searchbar/searchbar.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+interface SearchBarProps {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearch = () => {
+    onSearch(searchText);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Search"
+        value={searchText}
+        onChangeText={text => setSearchText(text)}
+      />
+      <Button title="Search" onPress={handleSearch} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginRight: 10,
+  },
+});
+
+export default SearchBar;
--- src/infrastructure/UI/screens/discovery.screen.tsx
+++ src/infrastructure/UI/screens/discovery.screen.tsx
@@ -1,9 +1,131 @@
-import { View, Text } from "react-native";
+import { SessionService } from "../../services/user/session.service";
+import { UserEntity } from "../../../domain/user/user.entity";
+import { useFocusEffect } from "@react-navigation/native";
+import { CRUDService } from "../../services/user/CRUD.service";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity, Button, FlatList } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import SearchBar from '../components/searchbar/searchbar';
 
 export default function DiscoveryScreen() {
+
+  const navigation = useNavigation();
+  const [currentUser, setCurrentUser] = useState<UserEntity | null>(null);
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const [userList, setUserList] = useState<UserEntity[] | null>(null);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      const getUser = async () => {
+        const userId = await SessionService.getCurrentUser();
+        console.log("ID of the Logged User:" + userId);
+        if (userId) {
+          try {
+            await CRUDService.getUser(userId)
+            .then((response) => {
+              console.log("Punto 1: " + response);
+              console.log(response?.data);
+              setCurrentUser(response?.data);
+            })
+          } catch (error) {
+            console.log("Error: " + error);
+          }
+        }
+      };
+      getUser();
+    }, [])
+  );
+
+  const handleSearch = async (query: string, setUserList: (users: UserEntity[]) => void) => {
+    console.log('He entrado en handleSearch.');
+    if (query.length > 0) {
+      try {
+        const response = await CRUDService.searchUsers(query);
+        console.log(response);
+        setUserList(response?.data);
+        console.log('He hecho el servicio de handleSearch.');
+        console.log('>> Lista de usuarios: ' + userList);
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      try {
+        const response = await CRUDService.getUsers();
+        setUserList(response?.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  const handleSearchWrapper = (searchText: string) => {
+    handleSearch(searchText, setUserList);
+  };
+
   return (
     <View>
-      <Text>Discovery</Text>
+      <Text>Welcome to Home Screen</Text>
+
+      <SearchBar onSearch={handleSearchWrapper} />
+       
+      <View style={styles.cardsUsers}>
+        {userList && userList.length > 0 ? (
+          <FlatList
+            data={userList}
+            keyExtractor={(user) => user.uuid}
+            renderItem={({ item: user }) => (
+              <TouchableOpacity
+                onPress={() => {
+                  if (currentUser?.uuid === user.uuid) {
+                    navigation.navigate('ProfileScreen' as never);
+                  } else {
+                    // IR A LA PÁGINA DONDE VER EL PERFIL DEL USUARIO
+                  }
+                }}
+                style={styles.userLink}
+              >
+                <View style={styles.user}>
+                  {user.photoUser ? (
+                    <Image source={{ uri: user.photoUser }} style={styles.userProfileImg} />
+                  ) : (
+                    <Image source={require('//ssl.gstatic.com/accounts/ui/avatar_2x.png')} style={styles.profileImgCard} />
+                  )}
+                  <View style={styles.userInfo}>
+                    <Text style={styles.userName}>{`${user.nameUser} ${user.surnameUser}`}</Text>
+                    <Text style={styles.userUsername}>@{user.appUser}</Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
+        ) : (
+          <Text style={styles.usersNotFound}>User Not Found</Text>
+        )}
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#000",
+  },
+  cardsUsers: {},
+  userLink: {},
+  user: {},
+  userProfileImg: {},
+  profileImgCard: {},
+  userInfo: {},
+  userName: {},
+  userUsername: {},
+  usersNotFound: {},
+});
