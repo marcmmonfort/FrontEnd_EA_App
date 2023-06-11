784647f8e0a065f93620f672836f8d14918fed32
VictorFernandezEscriva <Victor.Fernandez.Escriva@estudiantat.upc.edu>
2023-06-11 18:42:53 +0200

    fix: solution of dependencies errors

--- src/infrastructure/UI/components/calendar/calendar.tsx
+++ src/infrastructure/UI/components/calendar/calendar.tsx
@@ -1,22 +1,49 @@
-/*
-import React from 'react';
-import { View } from 'react-native';
-import { Calendar, ICalendarEventBase } from 'react-native-big-calendar';
-import { CalendarComponentProps } from './Types';
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { Calendar } from "react-native-calendars";
+import ActivityDetailsModal from "../activityDetails/activityModal";
+import { Activity } from "../../../../domain/activity/activity.entity";
 
+const CalendarScreen = () => {
+  const [selectedActivity, setSelectedActivity] = useState<Activity | null>(null);
+  const [listActivity, setListActivity] = useState<Activity[]>([]);
 
+  const handleDayPress = (date: { dateString: string }) => {
+    const clickedActivity = listActivity.find(
+      (activity) => activity.dateActivity.toISOString().split("T")[0] === date.dateString
+    );
+    if (clickedActivity) {
+      setSelectedActivity(clickedActivity);
+    } else {
+      setSelectedActivity(null);
+    }
+  };
+  
 
-const CalendarComponent = <T extends ICalendarEventBase>({
-  events,
-  height,
-  ...props
-}: CalendarComponentProps<T>) => {
-  /*return (
-    <View >
-      <Calendar<T> height={height} events={events} {...props} />
+  const closeActivityDetails = () => {
+    setSelectedActivity(null);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Calendar onDayPress={handleDayPress} />
+      {selectedActivity && (
+        <ActivityDetailsModal
+          activity={selectedActivity}
+          onClose={closeActivityDetails}
+        />
+      )}
     </View>
   );
-  */
+};
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
-//export default CalendarComponent;
+export default CalendarScreen;
--- src/infrastructure/UI/screens/calendar.screen.tsx
+++ src/infrastructure/UI/screens/calendar.screen.tsx
@@ -1,19 +1,63 @@
-import React from "react";
 import { View, Text, ImageBackground, StyleSheet } from "react-native";
+import { useState, useEffect } from 'react';
+import { SafeAreaView } from 'react-native';
+import { ActivityService } from "../../services/activity/activity.service";
+import { Activity } from "../../../domain/activity/activity.entity";
+import { SessionService } from "../../services/user/session.service";
+import React from "react";
+import Agenda from "../components/calendar/calendar";
 
-export default function CalendarEventsScreen() {
+
+function CalendarEventsScreen() {
+  const [actividades, setActividades] = useState<Activity[]>([]);
+  const [currentUser, setCurrentUser] = useState<string>(""); 
+
+  useEffect(() => {
+    //Obtenemos el usuario que ha iniciado sesión en la app
+    const fetchData = async () => {
+      setCurrentUser(await SessionService.getCurrentUser());
+
+      //Pedimos el horario de la semana del usuario logeado
+      const currentDate = new Date();
+      currentDate.setHours(0, 0, 0, 0);
+      console.log(currentDate);
+      const date = currentDate.toString();
+      const myScheduleResponse = await ActivityService.getMySchedule(currentUser, date);
+      if(myScheduleResponse){
+        setActividades(myScheduleResponse.data);
+      }
+    }
+    fetchData();
+    
+  }, []);
+  
+
   return (
     <ImageBackground source={require('../../../../assets/visualcontent/background_8.png')} style={styles.backgroundImage}>
-      <View>
-      <Text></Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <Agenda></Agenda>
+      </SafeAreaView>
     </ImageBackground>
   );
-}
+};
 
 const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
     resizeMode: 'cover',
   },
+  container: {
+    flex: 1,
+  },
+  eventoContainer: {
+    backgroundColor: 'lightblue',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  eventoText: {
+    fontSize: 16,
+  },
 });
+
+export default CalendarEventsScreen;
